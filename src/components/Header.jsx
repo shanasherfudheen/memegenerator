ec0ee0099173b09093c4559ab533e7664be80186
Shana Sherfudheen <shanasherfudheen@gmail.com>
2025-05-12 19:27:55 -0800

    Meme Generator

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import trollFace from '../assets/troll-face.png'
+
+function Header() {
+  return (
+    <header className="header">
+            <img 
+                src={trollFace} 
+            />
+            <h1>Meme Generator</h1>
+        </header>
+  )
+}
+
+export default Header
